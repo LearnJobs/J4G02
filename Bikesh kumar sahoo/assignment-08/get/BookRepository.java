b1967c9b5ec51e3477b34781f56498731e16d226
bickesh <101691049+bickesh@users.noreply.github.com>
2022-04-06 19:46:20 +0530

    Add files via upload

---
+++ Bikesh kumar sahoo/assignment-08/get/BookRepository.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+//@Repository
+//public interface BookRepository extends CrudRepository<Book, Integer>{
+//
+//}
+
+
+@Repository
+public interface BookRepository extends JpaRepository<Book,Integer>{
+
+}
