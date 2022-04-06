86431b73d485c9b38cea8abd8e5e23f306cd007f
Madhuri <101717105+7601061993@users.noreply.github.com>
2022-04-06 23:46:38 +0530

    Add files via upload

---
+++ Madhuri/BookException.java
@@ -0,0 +1,32 @@
+package com.BookException;
+import java.util.date;
+public class BookException {
+	private Date timestamp;
+	private String message;
+	private String details;
+	public Date getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Date timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public String getDetails() {
+		return details;
+	}
+	public void setDetails(String details) {
+		this.details = details;
+	}
+	public BookException(Date timestamp, String message, String details) {
+		super();
+		this.timestamp = timestamp;
+		this.message = message;
+		this.details = details;
+	}
+
+}
